2f2546e9d8571a5d90e602859e4ab0ea419c2d90
danarte <danarte@users.noreply.github.com>
2021-01-06 19:55:15 +0200

    updated location of fast5 files

--- Preparing_fast5_for_analysis/runAlbacore.sh
+++ Preparing_fast5_for_analysis/runAlbacore.sh
@@ -17,7 +17,7 @@ echo ""
 
 ## change to the correct location where ONT's albacore script is located
 ~/.local/bin/read_fast5_basecaller.py -r  --config r94_450bps_linear.cfg -k SQK-RAD004 -f FLO-MIN106  \
-    -o fast5 -s ./ -t $NCORES -i ./fast5_DatasetVol2/$FLOWCELLNAME -s basecalls/$FLOWCELLNAME
+    -o fast5 -s ./ -t $NCORES -i ./fast5/$FLOWCELLNAME -s basecalls/$FLOWCELLNAME
 set CURTIME=`date +"%m_%d_%T"`
 cp basecalls/$FLOWCELLNAME/pipeline.log basecalls/$FLOWCELLNAME/$CURTIME.pipeline.log
 cp basecalls/$FLOWCELLNAME/sequencing_summary.txt basecalls/$FLOWCELLNAME/$CURTIME.sequencing_summary.txt
